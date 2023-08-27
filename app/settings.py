ec94551d729402678fdb9b5d60d7c0944664030b
Juny2312 <tojuny2312@gmail.com>
2023-08-27 11:31:08 +0900

    before deploy

---
+++ app/settings.py
@@ -0,0 +1,9 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    debug: bool = False
+    environment: str
+    database_url: str
+
+    class Config:
+        env_file = ".env"
